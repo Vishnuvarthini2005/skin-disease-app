6c5c567024bb9895b363370faca7699ba9a71ee3
VISHNUVARTHINI MANOGARAN <144202346+Vishnuvarthini2005@users.noreply.github.com>
2026-09-13 11:38:49 +0530

    Initial commit - DermaScan AI app

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from PIL import Image
+import numpy as np
+import os
+import gdown
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+
+st.set_page_config(page_title="DermaScan AI", page_icon="🔬", layout="wide")
+
+MODEL_PATH = "best_classical_model.pth"
+FILE_ID = "1NyzHV2ny-J0cmVGGndLg_T_rh4XIbLla"
+
+if not os.path.exists(MODEL_PATH):
+    with st.spinner("Downloading model (first time only)..."):
+        gdown.download(f"https://drive.google.com/uc?id={FILE_ID}",
